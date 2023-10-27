7d25bea265c3d815866175218fce2b4f27af3531
DeveloperSuccess <megafreez@gmail.com>
2023-10-27 09:02:05 +0500

    Создал слой Application

---
+++ Src/PubSub.Application/PublisherSubscriber/Commands/AddValue/AddValueCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace PubSub.Application.PublisherSubscriber.Commands.AddValue
+{
+    public  record class AddValueCommand() : IRequest;    
+}
---
+++ Src/PubSub.Application/PublisherSubscriber/Commands/AddValue/AddValueCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace PubSub.Application.PublisherSubscriber.Commands.AddValue
+{
+    internal class AddValueCommandHandler : IRequestHandler<AddValueCommand>
+    {
+        public AddValueCommandHandler(IMediator mediator) 
+        { 
+        }        
+
+        Task IRequestHandler<AddValueCommand>.Handle(AddValueCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
