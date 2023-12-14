eb3575b193d3c6233790dbb3f2867abb496e01ea
DeveloperSuccess <megafreez@gmail.com>
2023-12-14 13:12:19 +0500

    Описание в Swagger

--- Src/PubSub.Api/Program.cs
+++ Src/PubSub.Api/Program.cs
@@ -1,6 +1,8 @@
+using Microsoft.OpenApi.Models;
 using PubSub.Application;
 using PubSub.Domain.Interfaces;
 using PubSub.Domain.Services;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +11,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(config =>
+{
+    config.CustomSchemaIds(type => type.ToString());
+    config.SwaggerDoc("v1", new OpenApiInfo { Title = "PubSub.Api", Version = "1.0" });
+
+    var currentAssembly = Assembly.GetExecutingAssembly();
+    var xmlDocs = currentAssembly.GetReferencedAssemblies()
+    .Union(new[] { currentAssembly.GetName() })
+    .Select(a => Path.Combine(Path.GetDirectoryName(currentAssembly.Location), $"{a.Name}.xml"))
+    .Where(f => File.Exists(f)).ToList();
+
+    xmlDocs.ForEach(xmlDoc => config.IncludeXmlComments(xmlDoc, true));
+});
 
 builder.Services.AddSingleton<IPublisherSubscriberManager, PublisherSubscriberManager>();
 
